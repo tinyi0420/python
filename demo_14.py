c67ca7c1ced48f7fbf1af45ea60685e76f46a3db
tinyi0420 <tina@jsun.com>
2019-11-15 15:16:44 +0800

    python baic lab with package

---
+++ demo_14.py
@@ -0,0 +1,10 @@
+v1 = 0
+v2 = 100
+v3 = -3000
+print(type(v1), type(v2), type(v3))
+print(v1 + 100, v1 - 100, v1 * 100, v1 / 100)
+print(2 ** 3, 3 ** 2)
+print(2 ** 512)
+v4 = 37
+print(f"v4={v4},as binary:{v4:b}")
+print(f"v4 as Oct={v4:o}, as hex={v4:x}")
