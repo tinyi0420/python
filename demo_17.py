e3f94ae510013fbbc44fe4333d2493a49ec60826
tinyi0420 <tina@jsun.com>
2019-11-15 16:04:19 +0800

    python baic lab with package

---
+++ demo_17.py
@@ -0,0 +1,13 @@
+a1 = 5 + 3j
+a2 = (-1) ** 0.5
+
+print(type(a1), a1)
+print(type(a2), a2)
+print(a1 + a2, a1 - a2, a1 * a2)
+print(a1 ** 2)
+print(abs(a1))
+
+x1 = a.real
+x2 = a1.imag
+print((x1 ** 2 + x2 ** 2) ** 0.5)
+print(a1.conjugate())
