14cde39db75afac991f55af6225d18d21e3430eb
tinyi0420 <tina@jsun.com>
2019-11-15 15:32:36 +0800

    python baic lab with package

---
+++ demo_15.py
@@ -0,0 +1,9 @@
+a1 = 3.14159
+a2 = 2.968
+print(a1, a2)
+print("%f,%f" % (a1 , a2))
+print("%.2f,%.2f" % (a1 , a2))
+print(round(1.1),round(1.4),round(1.5),round(1.6),round(1.9))
+print(round(2.1),round(2.4),round(2.5),round(2.6),round(2.9))
+print(round(3.1),round(3.4),round(3.5),round(3.6),round(3.9))
+
